509846c338d2fba3ffe00de7be242fda6bae278f
DerikDwiHeavyanto <hgiyant@gmail.com>
2021-09-10 14:47:45 +0700

    update commit

--- lib/screen/material/video.dart
+++ lib/screen/material/video.dart
@@ -7,32 +7,34 @@ class Video extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.fromLTRB(33, 86, 33, 0),
-      width: 295,
-      height: 203,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Color(0xFFFFFFFF),
-      ),
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              image: DecorationImage(
-                  image: AssetImage("assets/image7.png"), fit: BoxFit.cover),
-            ),
-          ),
-          Center(
-            child: Container(
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.fromLTRB(33, 86, 33, 0),
+        width: 295,
+        height: 203,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Color(0xFFFFFFFF),
+        ),
+        child: Stack(
+          children: [
+            Container(
               decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets/btn_play.png"))),
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                    image: AssetImage("assets/image7.png"), fit: BoxFit.cover),
+              ),
             ),
-          )
-        ],
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/btn_play.png"))),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
